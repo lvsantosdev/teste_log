3026e37b274e8bd24ee564a913ddf39647212e7e
lvsantosdev <105672045+lvsantosdev@users.noreply.github.com>
2024-01-31 00:28:53 -0300

    tratamento de excecoes e casos de testes
    tratamento de excecoes e casos de testes

--- api_leitura_arquivo_log.py
+++ api_leitura_arquivo_log.py
@@ -6,71 +6,86 @@ import util
 import init
 
 def retorna_json_erros_log():
+
+    try:
+        
+        ##variavel para check ao final da rotina se o arquivo de log existe no caminho definido e precisa ser fechado
+        arquivo_existe = True
      
-    ##abertura do arquivo
-    diretorio = Path(__file__).parent
-    diretorio_arquivo = str(diretorio) + '\log.txt'
-    arquivo_log = open(diretorio_arquivo, 'r', encoding = 'utf-8')
+        ##abertura do arquivo
+        diretorio = Path(__file__).parent
+        diretorio_arquivo = str(diretorio) + '\log.txt'
+        arquivo_log = open(diretorio_arquivo, 'r', encoding = 'utf-8')
 
-    ##recupera parametros
-    parametros = init.retorna_parametros()
+        ##recupera parametros
+        parametros = init.retorna_parametros()
 
-    ##separa as mensagens de erro no arquivo log no formato: função;mensagem_erro
-    erros = []
+        ##separa as mensagens de erro no arquivo log no formato: função;mensagem_erro
+        erros = []
 
-    for linha in arquivo_log:
-            if parametros['parm1'] in linha:
+        for linha in arquivo_log:
+                if parametros['parm1'] in linha:
 
-                pos_ini = linha.index(parametros['parm2']) + len(parametros['parm2'])
-                pos_final = linha.index(parametros['parm3'])
-                funcao = linha[pos_ini:pos_final]
+                    pos_ini = linha.index(parametros['parm2']) + len(parametros['parm2'])
+                    pos_final = linha.index(parametros['parm3'])
+                    funcao = linha[pos_ini:pos_final]
 
-                pos_ini = linha.index(parametros['parm3']) + len(parametros['parm3'])
-                pos_final = len(linha)
-                mensagem_erro = linha[pos_ini:pos_final].strip()
+                    pos_ini = linha.index(parametros['parm3']) + len(parametros['parm3'])
+                    pos_final = len(linha)
+                    mensagem_erro = linha[pos_ini:pos_final].strip()
 
-                erro_funcao = funcao + ';' + mensagem_erro
+                    erro_funcao = funcao + ';' + mensagem_erro
 
-                erros.append(erro_funcao)
+                    erros.append(erro_funcao)
 
-    ##inicia variaveis
-    mensagem_erro = ''
-    erro_funcao = ''
-    funcao = ''
-    funcao_anterior = ''
-    erros_funcao_anterior = []
-    erros_json = []
+        ##inicia variaveis
+        mensagem_erro = ''
+        erro_funcao = ''
+        funcao = ''
+        funcao_anterior = ''
+        erros_funcao_anterior = []
+        erros_json = []
 
-    ##ordena a lista para poder agrupar por função
-    erros.sort()
+        ##ordena a lista para poder agrupar por função
+        erros.sort()
 
-    ##loop pelos erros
-    for erro_funcao in  erros:
-        pos = erro_funcao.index(';')
-        funcao = erro_funcao[0:pos].strip()
-        mensagem_erro = erro_funcao[pos + 1:len(erro_funcao)].strip()
+        ##loop pelos erros
+        for erro_funcao in  erros:
+            pos = erro_funcao.index(';')
+            funcao = erro_funcao[0:pos].strip()
+            mensagem_erro = erro_funcao[pos + 1:len(erro_funcao)].strip()
 
-        ##valida se mudou a funcao em relacao o estado anterior
-        if funcao != funcao_anterior and funcao_anterior != "":
-            retorno_erros = util.agrupar_erros(erros_funcao_anterior)
-            erro_json = {funcao_anterior:{'quantidade_de_erros': retorno_erros[1], 'erros': retorno_erros[0]}}
-            erros_json.append(erro_json)
-            erros_funcao_anterior = []
-        funcao_anterior = funcao
-        erros_funcao_anterior.append(mensagem_erro)
+            ##valida se mudou a funcao em relacao o estado anterior
+            if funcao != funcao_anterior and funcao_anterior != "":
+                retorno_erros = util.agrupar_erros(erros_funcao_anterior)
+                erro_json = {funcao_anterior:{'quantidade_de_erros': retorno_erros[1], 'erros': retorno_erros[0]}}
+                erros_json.append(erro_json)
+                erros_funcao_anterior = []
+            funcao_anterior = funcao
+            erros_funcao_anterior.append(mensagem_erro)
 
-    ##apos finalizar o loop, adicionar o ultimo estado
-    retorno_erros = util.agrupar_erros(erros_funcao_anterior)
-    erro_json = {funcao_anterior:{'quantidade_de_erros': retorno_erros[1], 'erros': retorno_erros[0]}}
-    erros_json.append(erro_json)
+        ##apos finalizar o loop, adicionar o ultimo estado
+        retorno_erros = util.agrupar_erros(erros_funcao_anterior)
+        erro_json = {funcao_anterior:{'quantidade_de_erros': retorno_erros[1], 'erros': retorno_erros[0]}}
+        erros_json.append(erro_json)
 
-    ##transforma a saida em json
-    erros_json = json.dumps(erros_json)
+        ##transforma a saida em json
+        erros_json = json.dumps(erros_json)
 
-    ##fechar o arquivo ao final da rotina
-    arquivo_log.close()
+    except IOError:
+        ##exception para tratar caso o arquivo nao esteja no caminho definido
+        erros_json = {'Exception':'Arquivo de log nao encontrado'}
+        arquivo_existe = False
 
-    return erros_json
+    except Exception as e:
+        ##tratar exception diversas
+        erros_json = {'Exception': e}
+
+    finally:
+        ##fechar o arquivo ao final da rotina
+        if arquivo_existe:
+            arquivo_log.close()
+        return erros_json
 
 if __name__ == '__main__':
     erros_json = retorna_json_erros_log()
--- testes.py
+++ testes.py
@@ -11,7 +11,6 @@ def teste_total_erros(quantidade_esperada_erros):
     ##inicia variaveis de teste
     total_de_erros = 0
 
-    ##relatorio para o usuario
     for data in data_dict:
         for chave in data.keys():
             total_de_erros += data[chave]['quantidade_de_erros']
@@ -26,7 +25,6 @@ def teste_quantidade_erros_funcao(funcao_esperada, quantidade_esperada_erros):
     ##inicia variaveis de teste
     total_de_erros = 0
 
-    ##relatorio para o usuario
     for data in data_dict:
         for chave in data.keys():
             if funcao_esperada in chave:
@@ -34,7 +32,27 @@ def teste_quantidade_erros_funcao(funcao_esperada, quantidade_esperada_erros):
     
     return total_de_erros == quantidade_esperada_erros
 
+def teste_tipo_erros_funcao(funcao_esperada, erro_esperado, quantidade_esperada_erros):
+    # Converter a string em JSON em um dicionário
+    data_JSON = api_leitura_arquivo_log.retorna_json_erros_log()
+    data_dict = json.loads(data_JSON)
+
+    ##inicia variaveis de teste
+    total_de_erros = 0
+
+    for data in data_dict:
+        for chave in data.keys():
+            if funcao_esperada in chave:
+                total_de_erros += data[chave]['quantidade_de_erros']
+                for erro in data[chave]['erros']:
+                    if erro_esperado in erro.keys():
+                        total_de_erros = erro[erro_esperado]
+
+    return total_de_erros == quantidade_esperada_erros
+
 if __name__ == '__main__':
+
+    print('=====================================================')
     print('testes total de erro:')
     print(teste_total_erros(179))
     print(teste_total_erros(200))
@@ -42,9 +60,19 @@ if __name__ == '__main__':
     print(teste_total_erros('Leonardo'))
     print(teste_total_erros(-1))
 
-    print('============================')
+    print('=====================================================')
     print('testes erros por funcao')
     print(teste_quantidade_erros_funcao('GEDI_USB_StatusGet', 6))
     print(teste_quantidade_erros_funcao('GEDI_USB_StatusGet', 2))
+    print(teste_quantidade_erros_funcao('GEDI_USB_StatusGet', 'Leonardo@!#'))
     print(teste_quantidade_erros_funcao('GEDI_BT_Disconnect', 2))
     print(teste_quantidade_erros_funcao('GEDI_BT_Disconnect', 6))
+
+    print('=====================================================')
+    print('testes se encontrou o erro e a quantidade por funcao')
+    print(teste_tipo_erros_funcao('GEDI_BT_Disconnect', 'GEDI_RET_TEST_FAIL', 1))
+    print(teste_tipo_erros_funcao('GEDI_BT_Disconnect', 'GEDI_RET_TEST_FAIL', 4))
+    print(teste_tipo_erros_funcao('GEDI_BT_Disconnect', 'GEDI_RET_OK', -1))
+    print(teste_tipo_erros_funcao('GEDI_BT_Disconnect', 'GEDI_RET_OK', 2))
+    print(teste_tipo_erros_funcao('GEDI_BT_Disconnect', 'GEDI_RET_OK', 'Leo'))
+    print(teste_tipo_erros_funcao('GEDI_BT_Disconnect', 'leonardo', '3'))
